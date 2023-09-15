9e4c622a94c8f548c80f0dce21001938c801c746
Adam Cottrill <33773276+adacott@users.noreply.github.com>
2023-09-15 03:01:11 +0000

    Notes on js and brief assignment

---
+++ intro-to-js/assignment1.js
@@ -0,0 +1,14 @@
+console.log(23+97);
+console.log((23 + 97 + 2 + 5 + 6 + 7));
+console.log((4+6+9) / 77);
+
+let a = 10;
+console.log(a);
+console.log(a * 9);
+let b = 7 * a;
+console.log(b)
+
+const max = 57;
+actual = max - 13;
+percentage = actual/max;
+console.log(percentage);
