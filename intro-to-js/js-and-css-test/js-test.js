22b2e3544d4e3f488092927691c418a7b75ca361
Adam Cottrill <33773276+adacott@users.noreply.github.com>
2023-09-17 03:21:07 +0000

    added .gitignore for lint config

--- intro-to-js/js-and-css-test/js-test.js
+++ intro-to-js/js-and-css-test/js-test.js
@@ -89,8 +89,13 @@ const buttons = document.querySelectorAll('button');
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
 
-// and for each one we add a 'click' listener
-button.addEventListener('click', () => {
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        console.log(button.id);
+    });
+});
+
+button.addEventListener("click", () => {
     console.log(button.id);
-});
-});
+})
+
